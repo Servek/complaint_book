7075b1e670e969558984f99317bf7f09d8e3371f
Александр <avans0908@gmail.com>
2018-01-21 10:31:18 +0400

    Update_Visual1

--- ComplaintBook/Controllers/HomeController.cs
+++ ComplaintBook/Controllers/HomeController.cs
@@ -1,39 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using ComplaintBook.Models;
-using Microsoft.AspNetCore.Authorization;
-
-namespace ComplaintBook.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Diagnostics;using System.Linq;using System.Threading.Tasks;using Microsoft.AspNetCore.Mvc;using ComplaintBook.Models;using Microsoft.AspNetCore.Authorization;namespace ComplaintBook.Controllers{    public class HomeController : Controller    {        public IActionResult Index()        {            return View();        }        [Authorize]        public IActionResult About()        {            ViewData["Message"] = "Your application description page.";            return View();        }        public IActionResult Contact()        {            ViewData["Message"] = "Your contact page.";            return View();        }        public IActionResult Error()        {            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });        }    }}
