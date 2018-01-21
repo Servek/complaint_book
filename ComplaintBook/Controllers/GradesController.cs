5eae70aae3a9463c40f98ae28fc194a1d03bb3ce
Александр <avans0908@gmail.com>
2018-01-21 14:31:56 +0400

    Add_GradePreManagement

---
+++ ComplaintBook/Controllers/GradesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ComplaintBook.Models.DataBase;
+
+namespace ComplaintBook.Controllers
+{
+    public class GradesController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public GradesController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Grades
+        public async Task<IActionResult> Index()
+        {
+            var applicationContext = _context.Grades.Include(g => g.Employee);
+            return View(await applicationContext.ToListAsync());
+        }
+
+        // GET: Grades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Employee)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // GET: Grades/Create
+        public IActionResult CreateComplaint()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "LastName");
+            return View();
+        }
+
+        // GET: Grades/Create
+        public IActionResult CreateSuggestion()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "LastName");
+            return View();
+        }
+
+        // POST: Grades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SenderName,SenderEmail,IsAccepted,Message,InternalReportType,Score,EmployeeId")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(grade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "LastName", grade.EmployeeId);
+            return View(grade);
+        }
+
+        // GET: Grades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades.SingleOrDefaultAsync(m => m.Id == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "LastName", grade.EmployeeId);
+            return View(grade);
+        }
+
+        // POST: Grades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SenderName,SenderEmail,IsAccepted,Message,InternalReportType,Score,EmployeeId")] Grade grade)
+        {
+            if (id != grade.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(grade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GradeExists(grade.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "LastName", grade.EmployeeId);
+            return View(grade);
+        }
+
+        // GET: Grades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Employee)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var grade = await _context.Grades.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Grades.Remove(grade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GradeExists(int id)
+        {
+            return _context.Grades.Any(e => e.Id == id);
+        }
+    }
+}
--- ComplaintBook/Models/DataBase/Grade.cs
+++ ComplaintBook/Models/DataBase/Grade.cs
@@ -8,18 +8,23 @@ namespace ComplaintBook.Models.DataBase
         public int Id { get; set; }
 
         // Информация об отправителе
+        [Display(Name = "Имя отправителя")]
         [Required]
         public string SenderName { get; set; }
+        [Display(Name = "E-mail отправителя")]
         [Required]
         public string SenderEmail { get; set; }
+        [Display(Name = "Принято")]
         [Required]
         public bool IsAccepted { get; set; }
 
         // Текст отправления
+        [Display(Name = "Текст отправления")]
         [Required]
         public string Message { get; set; }
 
         // Тип отправления (жалоба или предложение)
+        [Display(Name = "Тип отправления")]
         [Required]
         [Column("ReportType")]
         public int InternalReportType { get; set; }
@@ -29,10 +34,13 @@ namespace ComplaintBook.Models.DataBase
             get { return (ReportType)this.InternalReportType; }
             set { this.InternalReportType = (int)value; }
         }
-        
+
+        [Display(Name = "Оценка сотрудника")]
         public int? Score { get; set; } // Оценка
 
+        [Display(Name = "Сотрудник")]
         public int? EmployeeId { get; set; }
+        [Display(Name = "Сотрудник")]
         public Employee Employee { get; set; } // Сотрудник
     }
 }
